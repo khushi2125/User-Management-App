1dde8e9bf5fda790b32dc1a4db9fd31d0de62574
khushi2125 <sharmakhushi2105@gmail.com>
2025-11-18 19:27:40 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
